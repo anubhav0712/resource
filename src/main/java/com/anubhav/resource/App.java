40d15f368e94edd306187eca60eb188864ccabde
anubhav0712 <vasudeshwal@gmail.com>
2019-08-04 20:04:34 +0530

    junit added

--- src/main/java/com/anubhav/resource/App.java
+++ src/main/java/com/anubhav/resource/App.java
@@ -21,16 +21,26 @@ public class App {
 		HashMap<String, HashMap<String, Float>> instances = objectMapper.readValue(new File("instances.json"),
 				new TypeReference<HashMap<String, HashMap<String, Float>>>() {
 				});
-		System.out.println("Instances loaded...");
+		System.out.println("Instances loaded...\nNote:Please provide input as follow , enter -1 to skip the input(not applicable for hours)");
 		System.out.print("Enter hours:");
 		int hours = in.nextInt();
+		if(hours<=0) {
+			System.out.println("hours should be greater than 0");
+			return;
+		}
 		System.out.print("Enter cpu:");
 		int cpu=in.nextInt();
-		System.out.println("Enter price");
+		System.out.print("Enter price:");
 		float price = in.nextFloat();
-		List<Response> response = allocator.get_costs(instances, hours, cpu, price);
+		if(cpu < 0 && price < 0) {
+			System.out.println("Provide correct input!");
+			return;
+		}
+		
+		List<Response> response = allocator.get_costs(instances, hours, cpu==-1?Integer.MIN_VALUE:cpu, price==-1?Float.MAX_VALUE:price);
 		Collections.sort(response, new ResponseComparator());
 		objectMapper.writeValue(new File("target/output.json"), response);
+		System.out.println("Please open target/output.json to see the output...");
 	}
 
 }
---
+++ src/test/java/com/anubhav/resource/AllocatorTest.java
@@ -0,0 +1,43 @@
+package com.anubhav.resource;
+
+import  org.junit.Assert;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.List;
+import org.junit.Test;
+
+import com.anubhav.resource.domain.Response;
+
+public class AllocatorTest {
+
+	@Test
+	public void testGetCost() {
+		Allocator allocator = new Allocator();
+		HashMap<String,HashMap<String,Float>> input = createInputData();
+		List<Response> list = allocator.get_costs(input, 1, 1, 1);
+		assertEquals(list.get(0).getTotal_cost().floatValue(), 0.894f,3);
+	}
+	
+	@Test
+	public void testGetCostForNoPriceInput() {
+		Allocator allocator = new Allocator();
+		HashMap<String,HashMap<String,Float>> input = createInputData();
+		List<Response> list = allocator.get_costs(input, 1, 1, Float.MAX_VALUE);
+		assertEquals(list.get(0).getTotal_cost().floatValue(), 0.12f,2);
+	}
+	
+	private HashMap<String,HashMap<String,Float>> createInputData(){
+		HashMap<String,HashMap<String,Float>> input = new HashMap<String,HashMap<String,Float>>();
+		HashMap<String,Float> ip1 = new HashMap<String,Float>();
+		ip1.put("large",0.12f);
+		ip1.put("xlarge",0.23f);
+		ip1.put("2xlarge",0.45f);
+		ip1.put("8xlarge",1.4f);
+		ip1.put("10xlarge",2.82f);
+		ip1.put("4xlarge",0.774f);
+		input.put("us-east", ip1);
+		return input;
+	}
+}
